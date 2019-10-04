9a74598d4efa40ce718ae0a446d543f06d51bfa4
Yogesh Kumar2 <Yogesh.Kumar2@rsystems.com>
2019-10-04 17:25:31 +0530

    ouath 2 commit

---
+++ Rocke t man/rocketMan/src/main/java/com/example/oauh/rocketMan/RocketManApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauh.rocketMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketManApplication.class, args);
+	}
+
+}
---
+++ spring-security-auth-client/spring-security-auth-client/src/main/java/com/techprimers/security/springsecurityauthclient/SpringSecurityAuthClientApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.security.springsecurityauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthClientApplication.class, args);
+	}
+
+}
